b2b8a0e9d590f2cf80f327a5753f7530d62ef9e5
4pcbr <me@4pcbr.com>
2013-12-18 19:49:24 +0400

    app created

---
+++ GOTO:AltSpace/DirectionData.h
@@ -0,0 +1,14 @@
+//
+//  DirectionData.h
+//  GOTO:AltSpace
+//
+//  Created by 4pcbr on 12/18/13.
+//  Copyright (c) 2013 AltSpace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DirectionData : NSObject
+
+@end
+
---
+++ GOTO:AltSpace/DirectionData.m
@@ -0,0 +1,13 @@
+//
+//  DirectionData.m
+//  GOTO:AltSpace
+//
+//  Created by 4pcbr on 12/18/13.
+//  Copyright (c) 2013 AltSpace. All rights reserved.
+//
+
+#import "DirectionData.h"
+
+@implementation DirectionData
+
+@end
