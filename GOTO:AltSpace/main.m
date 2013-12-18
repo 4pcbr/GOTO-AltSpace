279db0f43d99af2ecfdf500541e0a63e230bf1a5
4pcbr <me@4pcbr.com>
2013-12-18 19:38:26 +0400

    Initial Commit

---
+++ GOTO:AltSpace/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GOTO:AltSpace
+//
+//  Created by 4pcbr on 12/18/13.
+//  Copyright (c) 2013 AltSpace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
